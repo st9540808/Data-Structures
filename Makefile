7b95b6a3837dedc903c55fac4a1547d4bb32d1fc
st9540808 <st9540808@gmail.com>
2016-10-20 00:35:31 +0800

    DataStructures homeworl

---
+++ Makefile
@@ -0,0 +1,3 @@
+PROGRAM:
+	gcc -o test Programming_Homework2.c -std=c99 -g
+	./test
---
+++ hw2.c
@@ -0,0 +1,15 @@
+/**
+ *	DataSturctures
+ *	hw2
+ *	
+ *	Ch2 Aarray and Structures 
+ *	p.102 #9 "random walk" problem
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	
+	return 0;
+}
