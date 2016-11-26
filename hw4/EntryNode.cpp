f38f8db8955953a9f60222b7d120fde3bdd7f37d
st9540808 <st9540808@gmail.com>
2016-11-26 21:27:48 +0800

    overloading assignment operator & delete object code

--- hw4/EntryNode.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "EntryNode.h"
-
-EntryNode::EntryNode( const int &inputValue, const int &inputRow, const int &inputCol )
-	: value( inputValue ), row( inputRow ), col( inputCol ), down( NULL ), right( NULL )
-{
-
-}
-
