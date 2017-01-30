fcc62f4587d56532e9bb429b3de2e63d9771865a
st9540808 <st9540808@gmail.com>
2017-01-31 02:44:00 +0800

    change artistic style

--- Ch4 Linked Lists/main.cpp
+++ Ch4 Linked Lists/main.cpp
@@ -17,30 +17,31 @@ int main()
 
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 	std::default_random_engine generator(seed);
+
 	for (int i = 0; i <= n; i += h)
-	{	
+	{
 		SLL list_1(i);
-		
+
 		std::clock_t start_time1 = std::clock();
 		list_1.insertionSort();
-		double duration_insertionSort = 1000.0 * (clock() - start_time1) / CLOCKS_PER_SEC;
-		
+		double duration_insertionSort = 1000. * (clock() - start_time1) / CLOCKS_PER_SEC;
+
 		insertionSortTime << i << "," << duration_insertionSort << "\n";
-		cout << "insertionsort :" << i << "\n";
+		cout << "insertionsort : " << i << "\n";
 	}
-		
-	for (int i = 0; i <= n; i+= h)
+
+	for (int i = 0; i <= n; i += h)
 	{
 		SLL list_2(i);
-		
+
 		std::clock_t start_time2 = std::clock();
 		list_2.mergeSort();
-		double duration_rmergeSort = 1000.0 * (clock() - start_time2) / CLOCKS_PER_SEC;
-	
+		double duration_rmergeSort = 1000. * (clock() - start_time2) / CLOCKS_PER_SEC;
+
 		rmergeSortTime << i << "," << duration_rmergeSort << "\n";
-		cout << "rmergesort : "<< i << "\n";
+		cout << "rmergesort : " << i << "\n";
 	}
-	
+
 	insertionSortTime.close();
 	rmergeSortTime.close();
 	return 0;
