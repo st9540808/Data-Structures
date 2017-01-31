6fc786d71d63f39c2aa07d62de35b8a5ba438590
st9540808 <st9540808@gmail.com>
2017-01-31 21:54:58 +0800

    change main.cpp to SLLmain.cpp

---
+++ Ch4 Linked Lists/SLLmain.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <random>
+#include <chrono>
+#include <iomanip>
+#include <fstream>
+#include <ctime>
+#include "SLL.h"
+using namespace std::chrono;
+using namespace std;
+
+int main()
+{
+	int n = 2000, h = 1;
+	ofstream insertionSortTime, rmergeSortTime;
+	insertionSortTime.open("./plot/insertionSortTime.out");
+	rmergeSortTime.open("./plot/rmergeSortTime.out");
+
+	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+	std::default_random_engine generator(seed);
+
+	for (int i = 0; i <= n; i += h)
+	{
+		SLL list_1(i);
+
+		std::clock_t start_time1 = std::clock();
+		list_1.insertionSort();
+		double duration_insertionSort = 1000. * (clock() - start_time1) / CLOCKS_PER_SEC;
+
+		insertionSortTime << i << "," << duration_insertionSort << "\n";
+		cout << "insertionsort : " << i << "\n";
+	}
+
+	for (int i = 0; i <= n; i += h)
+	{
+		SLL list_2(i);
+
+		std::clock_t start_time2 = std::clock();
+		list_2.mergeSort();
+		double duration_rmergeSort = 1000. * (clock() - start_time2) / CLOCKS_PER_SEC;
+
+		rmergeSortTime << i << "," << duration_rmergeSort << "\n";
+		cout << "rmergesort : " << i << "\n";
+	}
+
+	insertionSortTime.close();
+	rmergeSortTime.close();
+	return 0;
+}
