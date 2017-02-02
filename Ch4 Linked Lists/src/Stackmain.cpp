5973b86b7c1aa971487a1370237ae1b9d61aa840
st9540808 <st9540808@gmail.com>
2017-02-02 20:13:42 +0800

    add Stack main

---
+++ Ch4 Linked Lists/src/Stackmain.cpp
@@ -0,0 +1,25 @@
+#include "Stack.h"
+using namespace std;
+
+int main()
+{
+	Stack myStack;
+	
+	myStack.push(3);
+	myStack.push(7);
+	myStack.push(1);
+	myStack.push(2);
+	myStack.push(5);
+	myStack.printStack();
+	cout << myStack.top() << endl;
+	
+	myStack.sortStack();
+	myStack.printStack();
+
+	cout << myStack.pop() << endl;
+	cout << myStack.pop() << endl;
+	cout << myStack.pop() << endl;
+	cout << myStack.isEmpty();
+
+	return 0;
+}
