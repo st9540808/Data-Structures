9e3f4c747ab66b5f41b41c9d34708092c8f1e9e2
st9540808 <st9540808@gmail.com>
2017-02-02 09:12:58 +0800

    simplified code by seperating measure function

--- Ch4 Linked Lists/src/SLLmain.cpp
+++ Ch4 Linked Lists/src/SLLmain.cpp
@@ -1,12 +1,5 @@
-#include <iostream>
-#include <random>
-#include <chrono>
-#include <iomanip>
-#include <fstream>
-#include <ctime>
 #include "SLL.h"
-using namespace std::chrono;
-using namespace std;
+#include "simple_measure.h"
 
 int main()
 {
@@ -15,32 +8,8 @@ int main()
 	insertionSortTime.open("./plot/insertionSortTime.out");
 	rmergeSortTime.open("./plot/rmergeSortTime.out");
 
-	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-	std::default_random_engine generator(seed);
-
-	for (int i = 0; i <= n; i += h)
-	{
-		SLL list_1(i);
-
-		std::clock_t start_time1 = std::clock();
-		list_1.insertionSort();
-		double duration_insertionSort = 1000. * (clock() - start_time1) / CLOCKS_PER_SEC;
-
-		insertionSortTime << i << "," << duration_insertionSort << "\n";
-		cout << "insertionsort : " << i << "\n";
-	}
-
-	for (int i = 0; i <= n; i += h)
-	{
-		SLL list_2(i);
-
-		std::clock_t start_time2 = std::clock();
-		list_2.mergeSort();
-		double duration_rmergeSort = 1000. * (clock() - start_time2) / CLOCKS_PER_SEC;
-
-		rmergeSortTime << i << "," << duration_rmergeSort << "\n";
-		cout << "rmergesort : " << i << "\n";
-	}
+	outputResult(insertionSortTime, &SLL::insertionSort, n, h);
+	outputResult(rmergeSortTime, &SLL::rmergeSort, n, h);
 
 	insertionSortTime.close();
 	rmergeSortTime.close();
